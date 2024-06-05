38a3cf38ba7a866196f64f6e3e1864fedd2ab1ab
Hunter King <hking6776@gmail.com>
2024-06-04 20:37:50 -0600

    cleantree

--- test/e2e/CommonCamelot.t.sol
+++ test/e2e/CommonCamelot.t.sol
@@ -2,12 +2,23 @@
 pragma solidity 0.8.20;
 
 import '@script/Registry.s.sol';
+import 'forge-std/Test.sol';
 import {Math} from '@opendollar/libraries/Math.sol';
+import {IAlgebraFactory} from '@algebra-core/interfaces/IAlgebraFactory.sol';
+import {IAlgebraPool} from '@algebra-core/interfaces/IAlgebraPool.sol';
 
-contract CommonCamelot {
-  function setUp() public virtual {}
+contract CommonCamelot is Test {
+  IAlgebraFactory public camelotFactory;
+  IAlgebraPool public camelotPool;
 
-  function _getPool() internal returns (address _pool) {
-    return address(0);
+  function setUp() public virtual {
+    camelotFactory = IAlgebraFactory(CAMELOT_FACTORY);
+    camelotPool = IAlgebraPool(camelotFactory.poolByPair(OD, WETH));
+  }
+}
+
+contract CommonCamelotTest is CommonCamelot {
+  function testCamelotPool() public {
+    assertEq(address(camelotPool), CAMELOT_OD_WETH_POOL);
   }
 }
