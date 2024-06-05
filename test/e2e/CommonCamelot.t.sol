b22ec917b15aa17291857d2ef76002333d774c83
Hunter King <hking6776@gmail.com>
2024-06-04 18:29:23 -0600

    add camelot addresses

---
+++ test/e2e/CommonCamelot.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+import {Common} from '@opendollar/test/e2e/Common.t.sol';
+import {Math} from '@opendollar/libraries/Math.sol';
+
+contract CommonCamelot is Common {}
