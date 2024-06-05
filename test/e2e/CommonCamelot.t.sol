1209565cd5c23288a8e1034aa770a7b5ea6d1698
Hunter King <hking6776@gmail.com>
2024-06-04 18:37:19 -0600

    cleantree

--- test/e2e/CommonCamelot.t.sol
+++ test/e2e/CommonCamelot.t.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
-import {Common} from '@opendollar/test/e2e/Common.t.sol';
+import '@script/Registry.s.sol';
 import {Math} from '@opendollar/libraries/Math.sol';
 
-contract CommonCamelot is Common {}
+contract CommonCamelot {
+  function setUp() public virtual {}
+
+  function _getPool() internal returns (address _pool) {
+    return address(0);
+  }
+}
