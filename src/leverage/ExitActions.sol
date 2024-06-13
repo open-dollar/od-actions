c15abf0f8293646f9c5aebd93336abf4d9bb49d1
Hunter King <hking6776@gmail.com>
2024-06-13 14:30:42 -0600

    pass tests

--- src/leverage/ExitActions.sol
+++ src/leverage/ExitActions.sol
@@ -15,7 +15,7 @@ contract ExitActions is BasicActions {
 
   /// @dev exits `_coinsToExit` system coins `_contract`
   function exitSystemCoinsToAccount(address _contract, address _coinJoin, uint256 _coinsToExit) external delegateCall {
-    _exitSystemCoinsToAccount(_contract, _coinJoin, _coinsToExit);
+    _exitSystemCoinsToAccount(_contract, _coinJoin, _coinsToExit * RAY);
   }
 
   /// @dev exits all system coins `_contract`
@@ -35,6 +35,21 @@ contract ExitActions is BasicActions {
     _generateDebtToAccount(_contract, _manager, _coinJoin, _safeId, _deltaWad);
   }
 
+  /// @dev generate debt to user proxy
+  function generateDebtToProxy(
+    address _manager,
+    address _coinJoin,
+    uint256 _safeId,
+    uint256 _deltaWad
+  ) external delegateCall {
+    _generateDebtToProxy(_manager, _coinJoin, _safeId, _deltaWad);
+  }
+
+  /// @dev generate debt without exit
+  function generateInternalDebt(address _manager, uint256 _safeId, uint256 _deltaWad) external delegateCall {
+    _generateInternalDebt(_manager, _safeId, _deltaWad);
+  }
+
   /// @dev lock collateral and generate debt to `_contract`
   function lockTokenCollateralAndGenerateDebtToAccount(
     address _contract,
@@ -50,25 +65,20 @@ contract ExitActions is BasicActions {
     );
   }
 
-  /// @dev Exits system coins from the safeEngine to `_contract`
+  /// @dev exits system coins from the safeEngine to `_contract`
   function _exitSystemCoinsToAccount(address _contract, address _coinJoin, uint256 _coinsToExit) internal virtual {
     if (_coinsToExit == 0) return;
-
     ICoinJoin __coinJoin = ICoinJoin(_coinJoin);
     ISAFEEngine __safeEngine = __coinJoin.safeEngine();
 
     if (!__safeEngine.canModifySAFE(address(this), _coinJoin)) {
       __safeEngine.approveSAFEModification(_coinJoin);
     }
-
     // transfer all coins to _contract
     __coinJoin.exit(_contract, _coinsToExit / RAY);
   }
 
-  /**
-   * @notice Generates debt
-   * @dev    Modifies the SAFE collateralization ratio, increasing the debt and sends the COIN amount to the user's address
-   */
+  /// @dev increase debt and send COIN amount to `_contract` address
   function _generateDebtToAccount(
     address _contract,
     address _manager,
@@ -80,12 +90,34 @@ contract ExitActions is BasicActions {
     ODSafeManager.SAFEData memory _safeInfo = ODSafeManager(_manager).safeData(_safeId);
 
     int256 deltaDebt = _getGeneratedDeltaDebt(_safeEngine, _safeInfo.collateralType, _safeInfo.safeHandler, _deltaWad);
-
-    // Generates debt in the SAFE
+    // generates debt in the SAFE
     _modifySAFECollateralization(_manager, _safeId, 0, deltaDebt, false);
-
     // exits and transfers COIN amount to address
     _collectAndExitCoinsToAccount(_contract, _manager, _coinJoin, _safeId, _deltaWad);
+  }
+
+  /// @dev increase debt and send COIN amount to user proxy
+  function _generateDebtToProxy(address _manager, address _coinJoin, uint256 _safeId, uint256 _deltaWad) internal {
+    address _safeEngine = ODSafeManager(_manager).safeEngine();
+    ODSafeManager.SAFEData memory _safeInfo = ODSafeManager(_manager).safeData(_safeId);
+
+    int256 deltaDebt = _getGeneratedDeltaDebt(_safeEngine, _safeInfo.collateralType, _safeInfo.safeHandler, _deltaWad);
+    // generates debt in the SAFE
+    _modifySAFECollateralization(_manager, _safeId, 0, deltaDebt, false);
+    // exits and transfers COIN amount to address
+    _collectAndTransferToProxy(_manager, _coinJoin, _safeId, _deltaWad);
+  }
+
+  /// @dev increase debt and does not send COIN amount, must exit manually
+  function _generateInternalDebt(address _manager, uint256 _safeId, uint256 _deltaWad) internal {
+    address _safeEngine = ODSafeManager(_manager).safeEngine();
+    ODSafeManager.SAFEData memory _safeInfo = ODSafeManager(_manager).safeData(_safeId);
+
+    int256 deltaDebt = _getGeneratedDeltaDebt(_safeEngine, _safeInfo.collateralType, _safeInfo.safeHandler, _deltaWad);
+    // generates debt in the SAFE
+    _modifySAFECollateralization(_manager, _safeId, 0, deltaDebt, false);
+    // moves the COIN amount to proxy's address
+    _transferInternalCoins(_manager, _safeId, address(this), _deltaWad * RAY);
   }
 
   /// @dev join collateral and exit an amount of COIN
@@ -100,17 +132,22 @@ contract ExitActions is BasicActions {
   ) internal {
     address _safeEngine = ODSafeManager(_manager).safeEngine();
     ODSafeManager.SAFEData memory _safeInfo = ODSafeManager(_manager).safeData(_safeId);
-
     // takes token amount from user's wallet and joins into the safeEngine
     _joinCollateral(_collateralJoin, _safeInfo.safeHandler, _collateralAmount);
 
     int256 deltaDebt = _getGeneratedDeltaDebt(_safeEngine, _safeInfo.collateralType, _safeInfo.safeHandler, _deltaWad);
-
     // locks token amount into the SAFE and generates debt
     _modifySAFECollateralization(_manager, _safeId, _collateralAmount.toInt(), deltaDebt, false);
-
     // exits and transfers COIN amount to address
     _collectAndExitCoinsToAccount(_contract, _manager, _coinJoin, _safeId, _deltaWad);
+  }
+
+  /// @dev transfer amount of COIN to proxy address
+  function _collectAndTransferToProxy(address _manager, address _coinJoin, uint256 _safeId, uint256 _deltaWad) internal {
+    // moves the COIN amount to proxy's address
+    _transferInternalCoins(_manager, _safeId, address(this), _deltaWad * RAY);
+    // exits the COIN amount to the user's proxy
+    _exitSystemCoinsToAccount(address(this), _coinJoin, _deltaWad * RAY);
   }
 
   /// @dev transfer amount of COIN to proxy address and exit to `_contract` address
--- test/CommonTest.t.sol
+++ test/CommonTest.t.sol
@@ -1,13 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
-import {Common, TKN} from '@opendollar/test/e2e/Common.t.sol';
-import {Math, RAY, RAD, WAD} from '@opendollar/libraries/Math.sol';
+import {Common} from '@opendollar/test/e2e/Common.t.sol';
+import {Math} from '@opendollar/libraries/Math.sol';
 import {ODProxy} from '@opendollar/contracts/proxies/ODProxy.sol';
-import {ERC20ForTest} from '@opendollar/test/mocks/ERC20ForTest.sol';
-import {IVault721} from '@opendollar/interfaces/proxies/IVault721.sol';
 import {ISAFEEngine} from '@opendollar/interfaces/ISAFEEngine.sol';
-import {IOracleRelayer} from '@opendollar/interfaces/IOracleRelayer.sol';
 import {ExitActions} from 'src/leverage/ExitActions.sol';
 
 contract CommonTest is Common {
@@ -35,6 +32,42 @@ contract CommonTest is Common {
     vaults[_proxy] = safeManager.openSAFE(_cType, _proxy);
   }
 
+  function _lockCollateral(bytes32 _cType, uint256 _safeId, uint256 _deltaWad, address _proxy) internal {
+    vm.startPrank(ODProxy(_proxy).OWNER());
+    bytes memory _payload = abi.encodeWithSelector(
+      exitActions.lockTokenCollateral.selector,
+      address(safeManager),
+      address(collateralJoin[_cType]),
+      _safeId,
+      _deltaWad
+    );
+    ODProxy(_proxy).execute(address(exitActions), _payload);
+    vm.stopPrank();
+  }
+
+  function _genDebtToAccount(address _contract, uint256 _safeId, uint256 _deltaWad, address _proxy) internal {
+    vm.startPrank(ODProxy(_proxy).OWNER());
+    bytes memory _payload = abi.encodeWithSelector(
+      exitActions.generateDebtToAccount.selector, _contract, address(safeManager), address(coinJoin), _safeId, _deltaWad
+    );
+    ODProxy(_proxy).execute(address(exitActions), _payload);
+  }
+
+  function _genDebtToProxy(uint256 _safeId, uint256 _deltaWad, address _proxy) internal {
+    vm.startPrank(ODProxy(_proxy).OWNER());
+    bytes memory _payload = abi.encodeWithSelector(
+      exitActions.generateDebtToProxy.selector, address(safeManager), address(coinJoin), _safeId, _deltaWad
+    );
+    ODProxy(_proxy).execute(address(exitActions), _payload);
+  }
+
+  function _genInternalDebt(uint256 _safeId, uint256 _deltaWad, address _proxy) internal {
+    vm.startPrank(ODProxy(_proxy).OWNER());
+    bytes memory _payload =
+      abi.encodeWithSelector(exitActions.generateInternalDebt.selector, address(safeManager), _safeId, _deltaWad);
+    ODProxy(_proxy).execute(address(exitActions), _payload);
+  }
+
   function _depositCollateralAndGenDebt(
     bytes32 _cType,
     uint256 _safeId,
@@ -44,7 +77,7 @@ contract CommonTest is Common {
   ) internal {
     vm.startPrank(ODProxy(_proxy).OWNER());
     bytes memory _payload = abi.encodeWithSelector(
-      basicActions.lockTokenCollateralAndGenerateDebt.selector,
+      exitActions.lockTokenCollateralAndGenerateDebt.selector,
       address(safeManager),
       address(collateralJoin[_cType]),
       address(coinJoin),
@@ -52,7 +85,7 @@ contract CommonTest is Common {
       _collatAmount,
       _deltaWad
     );
-    ODProxy(_proxy).execute(address(basicActions), _payload);
+    ODProxy(_proxy).execute(address(exitActions), _payload);
     vm.stopPrank();
   }
 
@@ -107,5 +140,11 @@ contract CommonTest is Common {
       abi.encodeWithSelector(exitActions.exitAllSystemCoinsToAccount.selector, _contract, address(coinJoin));
     ODProxy(_proxy).execute(address(exitActions), _payload);
     vm.stopPrank();
+  }
+
+  function _getSAFE(bytes32 _cType, address _safe) internal view returns (uint256 _collateral, uint256 _debt) {
+    ISAFEEngine.SAFE memory _safeData = safeEngine.safes(_cType, _safe);
+    _collateral = _safeData.lockedCollateral;
+    _debt = _safeData.generatedDebt;
   }
 }
