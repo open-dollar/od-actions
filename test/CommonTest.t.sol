fd3a8f321d0870fb3ede231e73e0f8c80312f673
Hunter King <hking6776@gmail.com>
2024-06-12 20:21:19 -0600

    add exitCoins test

---
+++ test/CommonTest.t.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+import {Common, TKN} from '@opendollar/test/e2e/Common.t.sol';
+import {Math, RAY, RAD, WAD} from '@opendollar/libraries/Math.sol';
+import {ODProxy} from '@opendollar/contracts/proxies/ODProxy.sol';
+import {ERC20ForTest} from '@opendollar/test/mocks/ERC20ForTest.sol';
+import {IVault721} from '@opendollar/interfaces/proxies/IVault721.sol';
+import {ISAFEEngine} from '@opendollar/interfaces/ISAFEEngine.sol';
+import {IOracleRelayer} from '@opendollar/interfaces/IOracleRelayer.sol';
+import {LeverageActions} from 'src/leverage/LeverageActions.sol';
+
+contract CommonTest is Common {
+  using Math for uint256;
+
+  address public aliceProxy;
+  address public bobProxy;
+  address public deployerProxy;
+
+  LeverageActions public leverageActions;
+
+  mapping(address proxy => uint256 safeId) public vaults;
+
+  function _deployOrFind(address _owner) internal returns (address) {
+    address proxy = vault721.getProxy(_owner);
+    if (proxy == address(0)) {
+      return address(vault721.build(_owner));
+    } else {
+      return proxy;
+    }
+  }
+
+  function _openSafe(address _proxy, bytes32 _cType) internal {
+    vm.prank(_proxy);
+    vaults[_proxy] = safeManager.openSAFE(_cType, _proxy);
+  }
+
+  function _depositCollateralAndGenDebt(
+    bytes32 _cType,
+    uint256 _safeId,
+    uint256 _collatAmount,
+    uint256 _deltaWad,
+    address _proxy
+  ) internal {
+    vm.startPrank(ODProxy(_proxy).OWNER());
+    bytes memory _payload = abi.encodeWithSelector(
+      basicActions.lockTokenCollateralAndGenerateDebt.selector,
+      address(safeManager),
+      address(collateralJoin[_cType]),
+      address(coinJoin),
+      _safeId,
+      _collatAmount,
+      _deltaWad
+    );
+    ODProxy(_proxy).execute(address(basicActions), _payload);
+    vm.stopPrank();
+  }
+
+  function _exitCoin(address _proxy, uint256 _amount) internal {
+    vm.startPrank(ODProxy(_proxy).OWNER());
+    bytes memory _payload = abi.encodeWithSelector(leverageActions.exitSystemCoins.selector, address(coinJoin), _amount);
+    ODProxy(_proxy).execute(address(leverageActions), _payload);
+    vm.stopPrank();
+  }
+
+  function _exitAllCoin(address _proxy) internal {
+    vm.startPrank(ODProxy(_proxy).OWNER());
+    bytes memory _payload = abi.encodeWithSelector(leverageActions.exitAllSystemCoins.selector, address(coinJoin));
+    ODProxy(_proxy).execute(address(leverageActions), _payload);
+    vm.stopPrank();
+  }
+
+  function _exitCoinToAccount(address _proxy, address _contract, uint256 _amount) internal {
+    vm.startPrank(ODProxy(_proxy).OWNER());
+    bytes memory _payload =
+      abi.encodeWithSelector(leverageActions.exitSystemCoinsToAccount.selector, _contract, address(coinJoin), _amount);
+    ODProxy(_proxy).execute(address(leverageActions), _payload);
+    vm.stopPrank();
+  }
+
+  function _exitAllCoinToAccount(address _proxy, address _contract) internal {
+    vm.startPrank(ODProxy(_proxy).OWNER());
+    bytes memory _payload =
+      abi.encodeWithSelector(leverageActions.exitAllSystemCoinsToAccount.selector, _contract, address(coinJoin));
+    ODProxy(_proxy).execute(address(leverageActions), _payload);
+    vm.stopPrank();
+  }
+}
--- test/e2e/E2ECoinExit.t.sol
+++ test/e2e/E2ECoinExit.t.sol
@@ -2,6 +2,49 @@
 pragma solidity 0.8.20;
 
 import '@script/Registry.s.sol';
-import {Common, TKN} from '@opendollar/test/e2e/Common.t.sol';
+import {IERC20} from '@openzeppelin/token/ERC20/IERC20.sol';
+import {TKN, WSTETH} from '@opendollar/test/e2e/Common.t.sol';
+import {MintableERC20} from '@opendollar/contracts/for-test/MintableERC20.sol';
+import {LeverageActions} from 'src/leverage/LeverageActions.sol';
+import {CommonTest} from 'test/CommonTest.t.sol';
 
-contract E2ECoinExit is Common {}
+contract E2ECoinExit is CommonTest {
+  uint256 public constant DEPOSIT = 10_000 ether;
+  uint256 public constant MINT = DEPOSIT * 2 / 3;
+
+  address public arbitraryContract;
+  address public token;
+
+  function setUp() public virtual override {
+    super.setUp();
+    leverageActions = new LeverageActions();
+    token = address(collateral[TKN]);
+
+    aliceProxy = _deployOrFind(alice);
+    _openSafe(aliceProxy, TKN);
+
+    MintableERC20(token).mint(alice, DEPOSIT);
+
+    vm.startPrank(alice);
+    IERC20(token).approve(aliceProxy, type(uint256).max);
+    _depositCollateralAndGenDebt(TKN, vaults[aliceProxy], DEPOSIT, MINT, aliceProxy);
+    vm.stopPrank();
+  }
+
+  function testExitCoinToUser() public {
+    uint256 _exitAmount = MINT / 2;
+    _exitCoin(aliceProxy, _exitAmount);
+    assertEq(systemCoin.balanceOf(alice), _exitAmount);
+  }
+
+  // function testExitAllCoinToUser() public {
+  //   vm.prank(alice);
+  //   leverageActions.exitAllSystemCoins(address(coinJoin));
+
+  //   assertEq(IERC20(token).balanceOf(alice), MINT);
+  // }
+
+  // function testExitCoinToArbitraryContract() public {}
+
+  // function testExitAllCoinToArbitraryContract() public {}
+}
