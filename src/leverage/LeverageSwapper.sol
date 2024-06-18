05d025b2d7483e4e42d22d07c4b2af7cea824dd5
Hunter King <hking6776@gmail.com>
2024-06-18 13:24:25 -0600

    cleantree

--- src/leverage/LeverageSwapper.sol
+++ src/leverage/LeverageSwapper.sol
@@ -6,5 +6,11 @@ import {PARASWAP_AUGUSTUS_SWAPPER} from '@script/ParaswapRegistry.s.sol';
 import {IParaswap} from 'src/leverage/interfaces/augustusV5/IParaswap.sol';
 
 contract LeverageSwapper {
-  IParaswap public constant paraswap = IParaswap(PARASWAP_AUGUSTUS_SWAPPER);
+  IParaswap public immutable PARASWAP_V5 = IParaswap(PARASWAP_AUGUSTUS_SWAPPER);
+
+  constructor(address _paraswapV5) {
+    PARASWAP_V5 = IParaswap(_paraswapV5);
+  }
+
+  function createOrder() public {}
 }
