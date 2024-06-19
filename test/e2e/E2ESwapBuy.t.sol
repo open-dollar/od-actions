c151fac45611adf41984dcb43df109db72d7eb84
Hunter King <hking6776@gmail.com>
2024-06-18 23:19:47 -0600

    testing

--- test/e2e/E2ESwapBuy.t.sol
+++ test/e2e/E2ESwapBuy.t.sol
@@ -12,7 +12,7 @@ import {ParaswapAdapter} from 'src/leverage/ParaswapAdapter.sol';
 
 contract E2ESwapBuy is BaseTest {
   ParaswapAdapter public paraswapAdapter;
-  mapping(bytes32 => address) internal _assets;
+  mapping(bytes32 => address) public assets;
 
   event Bought(address indexed fromAsset, address indexed toAsset, uint256 amountSold, uint256 receivedAmount);
 
@@ -25,31 +25,76 @@ contract E2ESwapBuy is BaseTest {
       IParaSwapAugustusRegistry(AugustusRegistry.ARBITRUM)
     );
 
-    _assets[RETH] = AaveV3ArbitrumAssets.rETH_UNDERLYING;
-    _assets[WETH] = AaveV3ArbitrumAssets.WETH_UNDERLYING;
-    _assets[WSTETH] = AaveV3ArbitrumAssets.wstETH_UNDERLYING;
+    assets[RETH] = AaveV3ArbitrumAssets.rETH_UNDERLYING;
+    assets[WETH] = AaveV3ArbitrumAssets.WETH_UNDERLYING;
+    assets[WSTETH] = AaveV3ArbitrumAssets.wstETH_UNDERLYING;
   }
 
   modifier buy(uint256 _amountToBuy) {
-    _amountToBuy = bound(amountToBuy, 1e15, 4000 ether);
+    _amountToBuy = bound(_amountToBuy, 1e15, 4000 ether);
     _;
   }
 
-  // static-tests
-  function fromRethToWeth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {
-    address assetToSwapFrom = _assets[fromAssetIndex];
-    address assetToSwapTo = _assets[toAssetIndex];
+  // FUZZ TESTS
+  function testFromRethToWeth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {
+    _swapBuy(assets[RETH], assets[WETH], _amountToBuy, _swapAll);
   }
 
-  function fromRethToWsteth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {}
+  // function testFromRethToWsteth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {
+  //   _swapBuy(assets[RETH], assets[WSTETH], _amountToBuy, _swapAll);
+  // }
 
-  function fromWstethToWeth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {}
+  // function testFromWstethToWeth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {
+  //   _swapBuy(assets[WSTETH], assets[WETH], _amountToBuy, _swapAll);
+  // }
 
-  function fromWstethToReth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {}
+  // function testFromWstethToReth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {
+  //   _swapBuy(assets[WSTETH], assets[RETH], _amountToBuy, _swapAll);
+  // }
 
-  function fromWethToReth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {}
+  // function testFromWethToReth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {
+  //   _swapBuy(assets[WETH], assets[RETH], _amountToBuy, _swapAll);
+  // }
 
-  function fromWethToWsteth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {}
+  // function testFromWethToWsteth(uint256 _amountToBuy, bool _swapAll) public buy(_amountToBuy) {
+  //   _swapBuy(assets[WETH], assets[WSTETH], _amountToBuy, _swapAll);
+  // }
 
-  // fuzz-test
+  // HELPER FUNCTION
+  function _swapBuy(address _fromAsset, address _toAsset, uint256 _amountToBuy, bool _swapAll) internal {
+    PsPResponse memory _psp = _fetchPSPRouteWithoutPspCacheUpdate({
+      from: _fromAsset,
+      to: _toAsset,
+      amount: _amountToBuy,
+      userAddress: user,
+      sell: false,
+      max: _swapAll
+    });
+
+    if (_swapAll) {
+      _checkAmountInParaSwapCalldata({offset: _psp.offset, amount: _amountToBuy, swapCalldata: _psp.swapCalldata});
+    }
+
+    deal(_fromAsset, address(paraswapAdapter), _psp.srcAmount);
+
+    uint256 beforeBalanceAdapter = IERC20Detailed(_fromAsset).balanceOf(address(paraswapAdapter));
+
+    vm.expectEmit(true, true, false, false, address(paraswapAdapter));
+    emit Bought(_fromAsset, _toAsset, _psp.srcAmount, _psp.destAmount);
+
+    paraswapAdapter.buyOnParaSwap({
+      toAmountOffset: _psp.offset,
+      paraswapData: abi.encode(_psp.swapCalldata, _psp.augustus),
+      assetToSwapFrom: IERC20Detailed(_fromAsset),
+      assetToSwapTo: IERC20Detailed(_toAsset),
+      maxAmountToSwap: _psp.srcAmount,
+      amountToReceive: _amountToBuy
+    });
+
+    uint256 afterBalanceAdapter = IERC20Detailed(_fromAsset).balanceOf(address(paraswapAdapter));
+
+    assertGe(_psp.srcAmount, beforeBalanceAdapter - afterBalanceAdapter, 'OVER-CONSUMED');
+    assertGt(_psp.destAmount, 0, 'ZERO-DST-AMOUNT');
+    assertGe(IERC20Detailed(_toAsset).balanceOf(address(paraswapAdapter)), _amountToBuy, 'RECEIVED BELOW QUOTE');
+  }
 }
